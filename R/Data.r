fceffd37c776b923f3bb25f277f168cb11fc8a5c
Victor Yuan <vyuan@BCRICWH.LAN>
2018-11-16 11:22:19 -0800

    subset down to 8/24 samples, to reduce size
    Former-commit-id: b220d2abeed1c539d68c863431c49ee32542d684 [formerly acfc8199d40d47a468cc088629950111ec017fa2]
    Former-commit-id: c4ddfced925ea4aeaafd7cde25a1fd511cbf3b89 [formerly 2f57b5c8ab45fa31bb3697e653df22bbf51bec1d]
    Former-commit-id: 60178964460924b57d28041c454a1c7e5f84058b

--- R/Data.r
+++ R/Data.r
@@ -7,20 +7,22 @@
 #'
 "pl_ethnicity_features"
 
-#' 24 DNA methylation profiles from preeclampsia and healthy control placentas
+#' 6 DNA methylation profiles from preeclampsia and healthy control placentas
 #'
 #' "Genome wide DNA methylation profiling of normal and preeclampsia placental samples. Illumina
 #' Infinium HumanMethylation450 BeadChip (450K array) was used to obtain DNA methylation profiles
 #' in placental samples. Samples included 16 samples from healthy uncomplicated pregnancies and 8
 #' samples from pregnancies affected by preeclampsia."
 #'
-#' These samples were downloaded from GEO and saved as an minfi rgset object.
+#' The full 24-sample dataset was downloaded from GEO and saved as an minfi rgset object. 2/24 were
+#' classified as Asians and the remaining 22/24 were Caucasian. To reduce memory usage, I filtered
+#' the sample size to 8 samples including the 2 samples classified as Asians.
 #'
 #' Reference: Yeung KR, Chiu CL, Pidsley R, Makris A et al. DNA methylation profiles in preeclampsia
 #' and healthy control placentas. Am J Physiol Heart Circ Physiol 2016 May 15;310(10):H1295-303.
 #' PMID: 26968548
 #'
-#' @format An rgset object with 24 samples
+#' @format An rgset object with 8 samples
 #' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196}
 #'
 "pl_rgset"
