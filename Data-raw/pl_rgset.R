01e194c448839c8860239312c2dd6dad834c6358
Victor Yuan <vyuan@BCRICWH.LAN>
2018-11-26 15:39:55 -0800

    formatting
    Former-commit-id: f756d02520d1c91f9a62851c8cfb9d8ff8d4fb34 [formerly 1ba67eb3d3d40bb71196e45e12e7f5b04ee702cc]
    Former-commit-id: 4c0631fe26e2d9b20696c338e34283168a559793 [formerly 901f4e20988beb6e233755f8b00c3714bcbc5409]
    Former-commit-id: e4902e62ec190f363db625cd47923401d013f15f

--- Data-raw/pl_rgset.R
+++ Data-raw/pl_rgset.R
@@ -15,4 +15,4 @@ sapply(idatFiles, gunzip, overwrite = TRUE)
 pl_rgset <- read.metharray.exp("GSE75196/idat")
 pl_rgset <- pl_rgset[,17:24] # save 8 samples
 
-use_data(pl_rgset, overwrite = T)
+use_data(pl_rgset, overwrite = T, internal = F)
