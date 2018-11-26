b2a4dbba302f09b013eff14796ed623fd8742760
Victor Yuan <vyuan@BCRICWH.LAN>
2018-11-26 15:39:33 -0800

    ethnic features in sysdata.rda
    Former-commit-id: d6ab5299862062ca8c38960108f593f870066749 [formerly 8b4c4bbab92071cdd12b8a6f74163d302ed32c2d]
    Former-commit-id: 3c38db935d5affe7ed8d80114d2cfdf1ca1129ce [formerly c2b49c6487058acee00f2da503938df36e08ab7a]
    Former-commit-id: 85e3bd3dadc80d25a3dac50a4576a2c2376ac305

--- Data-raw/pl_glmfit.R
+++ Data-raw/pl_glmfit.R
@@ -4,6 +4,7 @@
 # load object from local disc
 pl_glmnet <- readRDS('Z:/Victor/Projects/DNAm-Ethnicity-Predictor/Robjects_final/05_glm_fit.rds')
 
+############ model parameters
 # a0
 a0 <- pl_glmnet$a0
 rownames(a0) <- rep("(Intercept)", nrow(a0))
@@ -22,5 +23,17 @@ lamlist <- glmnet::lambda.interp(lambda, 0.01) # or this
 s <- pl_glmnet$lambdaOpt
 nlambda <- length(s)
 
-#save
-use_data(a0, nclass, nbeta, lamlist, s, nlambda, internal = T, overwrite = T)
+############### pull out the coefficients from glmnet model
+coef <- coef(pl_glmnet, pl_glmnet$lambdaOpt)
+coef <- data.frame(do.call("cbind", lapply(coef, function(x) x[,1])) )
+
+# Remove coefficients = to 0
+non_zero <- abs(rowSums(coef)) > 0
+coef <- coef[non_zero,] # 1860, drops intercept automatically
+
+# take out feature names
+pl_ethnicity_features <- rownames(coef)
+
+############### save
+usethis::use_data(pl_ethnicity_features, a0, nclass, nbeta, lamlist, s, nlambda,
+                   internal = T, overwrite = T)
