86d46fb4fd119daed53c76521f7ab22ab7a3878c
Victor <victor.2wy@gmail.com>
2020-02-03 17:05:43 -0800

    add cell reference cpgs to pl_betas
    Former-commit-id: 935a6df5efa52c3a93f2b04a57d50916a5cbc126 [formerly bf2337e24dd526b001176b7b2143f41f839591d1 [formerly 7361345e9c3c668950fc246c7527ebc216f34a9b]]
    Former-commit-id: 8f89ea63c489763c93a7210b4ae270cab236a07d [formerly 49840d072b8de064b6d44ad9e7d01cb8c01e02cf]
    Former-commit-id: d84f1d0205584b434d2ea3fdcd9fe45bf81bc04a
    Former-commit-id: 76f360af604b26c5f343dc562f9a247d5908b416

--- Data-raw/pl_cell_cpgs.R
+++ Data-raw/pl_cell_cpgs.R
@@ -7,5 +7,14 @@ pl_cell_cpgs_third <-
 pl_cell_cpgs_first <-
   read_csv('Z:/Victor/Projects/NIH - cells/Manuscript/data/2_14_deconvolution_reference_cpgs_first.csv')
 
+# convert to matrix
+pl_cell_cpgs_third <- pl_cell_cpgs_third[,2:6] %>%
+  as.matrix() %>%
+  magrittr::set_rownames(pl_cell_cpgs_third$cpg)
+
+pl_cell_cpgs_first <- pl_cell_cpgs_first[,2:6] %>%
+  as.matrix() %>%
+  magrittr::set_rownames(pl_cell_cpgs_first$cpg)
+
 
 usethis::use_data(pl_cell_cpgs_third, pl_cell_cpgs_first, overwrite = TRUE, internal = FALSE)
