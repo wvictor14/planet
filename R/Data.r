b8f34f8c4c86ab699ae745df7cd396761dc8de78
Victor <victor.2wy@gmail.com>
2020-02-03 16:40:37 -0800

    add reference cell cpgs
    Former-commit-id: bad34a189763cf9ec3578a146ebd616e68c3ac96 [formerly 8b8c0f0de0153d888b8fdac3d8f8da35189129a7]
    Former-commit-id: fb86cc629af72d3322d60d234f9e554ec02ac873 [formerly 00aa82978486c6abe41d921886f71fd37b21cab3]
    Former-commit-id: 9f7f47e99de61355591d69d3a9abc88637adb22c

--- R/Data.r
+++ R/Data.r
@@ -14,9 +14,9 @@
 #' in placental samples. Samples included 16 samples from healthy uncomplicated pregnancies and 8
 #' samples from pregnancies affected by preeclampsia."
 #'
-#' The DNA methylation data for 24 placental samples were downloaded from 
-#' [GSE75196](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196). After normalizing using 
-#' `minfi::preprocessNoob` and `wateRmelon::BMIQ`, the betas were filtered to 10,000 random CpGs + 
+#' The DNA methylation data for 24 placental samples were downloaded from
+#' [GSE75196](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196). After normalizing using
+#' `minfi::preprocessNoob` and `wateRmelon::BMIQ`, the betas were filtered to 10,000 random CpGs +
 #' those CpGs used in the gestational age clock and ethnicity classifier.
 #'
 #' Reference: Yeung KR, Chiu CL, Pidsley R, Makris A et al. DNA methylation profiles in preeclampsia
@@ -28,7 +28,7 @@
 #'
 "pl_betas"
 
-#' Sex, disease, and gestational age information associated with `pl_betas`. Taken from 
+#' Sex, disease, and gestational age information associated with `pl_betas`. Taken from
 #' [GSE75196](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196).
 #'
 #' @format A [tibble][tibble::tibble-package]
@@ -37,11 +37,30 @@
 "pl_pDat"
 
 #' Coefficients from the three placental gestational age clocks from Lee Y et al. 2019.
-#' 
-#' Reference: Lee Y, Choufani S, Weksberg R, et al. Placental epigenetic clocks: estimating 
+#'
+#' Reference: Lee Y, Choufani S, Weksberg R, et al. Placental epigenetic clocks: estimating
 #' gestational age using placental DNA methylation levels. Aging (Albany NY). 2019;11(12):4238–4253.
 #'  doi:10.18632/aging.102049.
 #' PMID: 31235674
-#' 
+#'
 #' @format A [tibble][tibble::tibble-package] with coefficients for the RPC, CPC, and refined RPC.
 "pl_clock"
+
+#' First trimester coefficients for placental cellular deconvolution from Yuan V et al. 2020.
+#'
+#' Reference:
+#' PMID:
+#'
+#' @format A [tibble][tibble::tibble-package] with coefficients for Trophoblasts, Stromal,
+#' Endothelial, Hofbauer cells, and nRBCs.
+"pl_cell_cpgs_first"
+
+#' Third trimester coefficients for placental cellular deconvolution from Yuan V et al. 2020.
+#'
+#' Reference:
+#' PMID:
+#'
+#' @format A [tibble][tibble::tibble-package] with coefficients for Trophoblasts, Stromal,
+#' Endothelial, Hofbauer cells, and nRBCs.
+"pl_cell_cpgs_third"
+
