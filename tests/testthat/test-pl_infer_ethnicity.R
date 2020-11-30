b5570b1ae00c1e13db8f04cc042772fe47f8d73a
Victor <victor.2wy@gmail.com>
2020-11-30 15:04:04 -0800

    add tests for pl_infer_ethnicity

---
+++ tests/testthat/test-pl_infer_ethnicity.R
@@ -0,0 +1,18 @@
+test_that("pl_infer_ethnicity() returns data.frame", {
+  expect_true(is.data.frame(pl_infer_ethnicity(pl_betas)))
+})
+
+test_that("pl_infer_ethnicity() works when some CpGs missing", {
+  pl_betas_missing_cpgs <- pl_betas[1:1500,]
+  expect_true(is.data.frame(pl_infer_ethnicity(pl_betas_missing_cpgs)))
+})
+
+test_that("pl_infer_ethnicity() output contains all columns", {
+  output_colnames <- names(pl_infer_ethnicity(pl_betas))
+  expect_true(
+    all(
+      c("Predicted_ethnicity_nothresh", "Predicted_ethnicity",
+        "Prob_African", "Prob_Asian", "Prob_Caucasian", "Highest_Prob") %in%
+        output_colnames))
+})
+
