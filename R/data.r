fb549e4f16ea38330f712f1d9d903e03b245df29
Victor <victor.2wy@gmail.com>
2020-12-11 10:28:26 -0800

    wrap line at 80 chars
    Former-commit-id: 2667a4aa1573a4db335f3c477f60e8af4546f117 [formerly 9236593ad6d5a5b5034c8940c37cf3e9b545b27e] [formerly 2558753a49fa6b77e176185381ed2a400e30e829] [formerly 2f8da5856fcd327570cb11b7db07e8d796340e4c [formerly 3a8d2aba500e81f33bb178c344527ca84fbacfa2 [formerly 30671121868b423206b12c8adce21103c84bc717]]]
    Former-commit-id: e1012b7b0e73a506570f640e21c4fd24336f5db6 [formerly 1c431fc7e588c1361bbde4a364563b49027100d7] [formerly 00a8017090b5b699e3904197ea9675a06de72ac0] [formerly 128c4530203a86a279e328e5e5cad696e1a4698e [formerly be366f37ed3cc826c81f95bca3aab0563a41002a]]
    Former-commit-id: b15ae6e1517c3dec00af3272f2a85e9e6388eb0d [formerly 46927f8c2ac13413ffa52cff321724727c89861b]
    Former-commit-id: 8dffcac4e71c1408672dd6aa8e01de602a8f6f36
    Former-commit-id: 63f7478398cf536a23c7b1116c0763af10fb6c7a
    Former-commit-id: bc7a9ede707f7b5d5eb80429623bce265d7c8ad4

--- R/data.r
+++ R/data.r
@@ -9,18 +9,21 @@
 
 #' 6 DNA methylation profiles from preeclampsia and healthy control placentas
 #'
-#' "Genome wide DNA methylation profiling of normal and preeclampsia placental samples. Illumina
-#' Infinium HumanMethylation450 BeadChip (450K array) was used to obtain DNA methylation profiles
-#' in placental samples. Samples included 16 samples from healthy uncomplicated pregnancies and 8
-#' samples from pregnancies affected by preeclampsia."
+#' "Genome wide DNA methylation profiling of normal and preeclampsia placental 
+#' samples. Illumina Infinium HumanMethylation450 BeadChip (450K array) was used
+#' to obtain DNA methylation profiles in placental samples. Samples included 16 
+#' samples from healthy uncomplicated pregnancies and 8 samples from pregnancies
+#'  affected by preeclampsia."
 #'
 #' The DNA methylation data for 24 placental samples were downloaded from
-#' [GSE75196](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196). After normalizing using
-#' `minfi::preprocessNoob` and `wateRmelon::BMIQ`, the betas were filtered to 10,000 random CpGs +
-#' those CpGs used in the gestational age clock and ethnicity classifier.
+#' [GSE75196](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196). 
+#' After normalizing using `minfi::preprocessNoob` and `wateRmelon::BMIQ`, the 
+#' betas were filtered to 10,000 random CpGs + those CpGs used in the 
+#' gestational age clock and ethnicity classifier.
 #'
-#' Reference: Yeung KR, Chiu CL, Pidsley R, Makris A et al. DNA methylation profiles in preeclampsia
-#' and healthy control placentas. Am J Physiol Heart Circ Physiol 2016 May 15;310(10):H1295-303.
+#' Reference: Yeung KR, Chiu CL, Pidsley R, Makris A et al. DNA methylation 
+#' profiles in preeclampsia and healthy control placentas. Am J Physiol Heart 
+#' Circ Physiol 2016 May 15;310(10):H1295-303.
 #' PMID: 26968548
 #'
 #' @format A matrix
@@ -29,24 +32,27 @@
 "pl_betas"
 
 #' Sex, disease, and gestational age information associated with `pl_betas`. 
-#' Taken from [GSE75196](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196).
+#' Taken from 
+#' [GSE75196](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196).
 #'
 #' @format A [tibble][tibble::tibble-package]
 #' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75196}
 #'
 "pl_pDat"
 
-#' Coefficients from the three placental gestational age clocks from Lee Y et al. 2019.
+#' Coefficients from the three placental gestational age clocks from Lee Y et 
+#' al. 2019.
 #'
-#' Reference: Lee Y, Choufani S, Weksberg R, et al. Placental epigenetic clocks: estimating
-#' gestational age using placental DNA methylation levels. Aging (Albany NY). 2019;11(12):4238–4253.
-#'  doi:10.18632/aging.102049.
-#' PMID: 31235674
+#' Reference: Lee Y, Choufani S, Weksberg R, et al. Placental epigenetic clocks:
+#' estimating gestational age using placental DNA methylation levels. Aging 
+#' (Albany NY). 2019;11(12):4238–4253. doi:10.18632/aging.102049. PMID: 31235674
 #'
-#' @format A [tibble][tibble::tibble-package] with coefficients for the RPC, CPC, and refined RPC.
+#' @format A [tibble][tibble::tibble-package] with coefficients for the RPC, 
+#' CPC, and refined RPC.
 "pl_clock"
 
-#' First trimester coefficients for placental cellular deconvolution from Yuan V et al. 2020.
+#' First trimester coefficients for placental cellular deconvolution from Yuan V
+#' et al. 2020.
 #'
 #' Reference: to be edited
 #' PMID: to be edited
@@ -55,7 +61,8 @@
 #' Endothelial, Hofbauer cells, nRBCs, and Syncytiotrophoblasts.
 "pl_cell_cpgs_first"
 
-#' Third trimester coefficients for placental cellular deconvolution from Yuan V et al. 2020.
+#' Third trimester coefficients for placental cellular deconvolution from Yuan V
+#' et al. 2020.
 #'
 #' Reference: to be edited
 #' PMID: to be edited
