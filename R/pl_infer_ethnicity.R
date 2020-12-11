936aab5fdf9f5cdda7d7e6e6f5ae0292813c47c7
Victor <victor.2wy@gmail.com>
2020-12-11 13:48:23 -0800

    update docs to address bioccheck warning
    Former-commit-id: b9d57bdbf6a7e822fbb1a1fc27d8c807c5784e50 [formerly 229378e9b5490c0a28d7650100f70de8cd64342a] [formerly a4e89843271303f6a21db169a49d7fe89e29d7f8] [formerly 4dcd713b472aeacbd7ae2a4af913e975f9dcd053 [formerly 6c69801396dc9b6328baf272f967a923b856b6f4]]
    Former-commit-id: e30501eae615f46581355b03582bd01367e9ddf4 [formerly 8bad468e520ea86bb4f1305f203847a29a65c3ae] [formerly 9ae47efe33afbbf087f55dc06c16f35390927089] [formerly 6579605de1aa2c779317dd0b1da79c3936053daf [formerly 0998a0785b1fc134f7aab42fb599a484a1fbacc0]]
    Former-commit-id: a6f8bef9c5079105fa6a1b1bbfbfeb74fa941d1e [formerly f6f6192aa523fc1373f39e1664a4e5b304ccfdfb]
    Former-commit-id: d3d756560a48e50d55fd48a4f912fdd62a1bc9f2
    Former-commit-id: ec0b4537d6f1e437e7c2711cbb1759cee8180172

--- R/pl_infer_ethnicity.R
+++ R/pl_infer_ethnicity.R
@@ -1,19 +1,23 @@
 #' Infers ethnicity using DNA methylation microarray data
 #'
-#' \code{pl_infer_ethnicity} Uses a glmnet model to predict ethnicity using DNA methylation data.
+#' \code{pl_infer_ethnicity} Uses a glmnet model to predict ethnicity using DNA 
+#' methylation data.
 #'
-#' @param betas n x m dataframe of methylation values on the beta scale (0, 1), where the variables
-#' are arranged in rows, and samples in columns. Should contain all 1860 predictors and be
-#' normalized with NOOB and BMIQ.
-#' @param threshold A probability threshold ranging from (0, 1) to call samples 'ambiguous'.
-#' Defaults to 0.75.
-#' @details Predicts self-reported ethnicity from 3 classes: Africans, Asians, and Caucasians, using
-#' placental DNA methylation data measured on the Infinium 450k/EPIC methylation array. Will return
-#' membership probabilities that often reflect genetic ancestry composition.
+#' @param betas n x m dataframe of methylation values on the beta scale (0, 1), 
+#' where the variables are arranged in rows, and samples in columns. Should 
+#' contain all 1860 predictors and be normalized with NOOB and BMIQ.
+#' @param threshold A probability threshold ranging from (0, 1) to call samples 
+#' 'ambiguous'. Defaults to 0.75.
+#' @details Predicts self-reported ethnicity from 3 classes: Africans, Asians, 
+#' and Caucasians, using placental DNA methylation data measured on the Infinium
+#' 450k/EPIC methylation array. Will return membership probabilities that often 
+#' reflect genetic ancestry composition.
 #'
-#' The input data should contain all 1860 predictors (cpgs) of the final GLMNET model.
+#' The input data should contain all 1860 predictors (cpgs) of the final GLMNET 
+#' model.
 #'
-#' It's recommended to use the same normalization methods used on the training data: NOOB and BMIQ.
+#' It's recommended to use the same normalization methods used on the training 
+#' data: NOOB and BMIQ.
 #'
 #' @return a [tibble][tibble::tibble-package]
 #' @examples
@@ -22,85 +26,90 @@
 #' # Load placenta DNAm data
 #' data(pl_betas)
 #' pl_infer_ethnicity(pl_betas)
-#'
 #' @export
 #'
-pl_infer_ethnicity <- function(betas, threshold = 0.75){
-
+pl_infer_ethnicity <- function(betas, threshold = 0.75) {
   pf <- intersect(rownames(betas), planet::pl_ethnicity_features)
-  if(length(pf) < length(planet::pl_ethnicity_features)){
-    warning(paste('Only', length(pf), 'out of', length(planet::pl_ethnicity_features), 'present.'))
+  if (length(pf) < length(planet::pl_ethnicity_features)) {
+    warning(paste("Only", length(pf), "out of", 
+                  length(planet::pl_ethnicity_features), "present."))
   } else {
-    print(paste(length(pf), 'of 1860 predictors present.'))
+    print(paste(length(pf), "of 1860 predictors present."))
   }
 
   # subset down to 1860 final features
-  betas <- t(betas[pf,])
+  betas <- t(betas[pf, ])
 
-  #glmnet code:
+  # glmnet code:
   for (i in seq(nclass)) {
-    kbeta <- methods::rbind2(a0[i, , drop = FALSE], nbeta[[i]])#was rbind2
+    kbeta <- methods::rbind2(a0[i, , drop = FALSE], nbeta[[i]]) # was rbind2
     vnames <- dimnames(kbeta)[[1]]
     dimnames(kbeta) <- list(NULL, NULL)
-    kbeta <- kbeta[, lamlist$left, drop = FALSE] %*% Matrix::Diagonal(x=lamlist$frac) +
-      kbeta[, lamlist$right, drop = FALSE] %*% Matrix::Diagonal(x=1 - lamlist$frac)
+    kbeta <- kbeta[, lamlist$left, drop = FALSE] %*% 
+      Matrix::Diagonal(x = lamlist$frac) +
+      kbeta[, lamlist$right, drop = FALSE] %*% 
+      Matrix::Diagonal(x = 1 - lamlist$frac)
     dimnames(kbeta) <- list(vnames, paste(seq(along = s)))
     nbeta[[i]] <- kbeta
   }
 
-  if (inherits(betas, "sparseMatrix"))
+  if (inherits(betas, "sparseMatrix")) {
     betas <- methods::as(betas, "dgCMatrix")
-  npred <-nrow(betas) # number of samples
+  }
+  npred <- nrow(betas) # number of samples
   dn <- list(names(nbeta), dimnames(nbeta[[1]])[[2]], dimnames(betas)[[1]])
-  dp <- array(0, c(nclass, nlambda, npred), dimnames = dn) # set up for results output
+  dp <- array(0, c(nclass, nlambda, npred), dimnames = dn) # set up for results
 
   # cross product with coeeficients
   for (i in seq(nclass)) {
-    fitk <- methods::cbind2(1, betas) %*% matrix(nbeta[[i]][c("(Intercept)", colnames(betas)),])
-    dp[i, , ] = dp[i, , ] + t(as.matrix(fitk))
+    fitk <- methods::cbind2(1, betas) %*% 
+      matrix(nbeta[[i]][c("(Intercept)", colnames(betas)), ])
+    dp[i, , ] <- dp[i, , ] + t(as.matrix(fitk))
   }
 
-  #probabilities
+  # probabilities
   pp <- exp(dp)
   psum <- apply(pp, c(2, 3), sum)
-  probs <- data.frame(aperm(pp/rep(psum, rep(nclass, nlambda * npred)), c(3, 1, 2)))
-  colnames(probs) <- paste0('Prob_', dn[[1]])
+  probs <- data.frame(aperm(pp / rep(psum, rep(nclass, nlambda * npred)), 
+                            c(3, 1, 2)))
+  colnames(probs) <- paste0("Prob_", dn[[1]])
 
-  #classification
+  # classification
   link <- aperm(dp, c(3, 1, 2))
   dpp <- aperm(dp, c(3, 1, 2))
   preds <- data.frame(apply(dpp, 3, glmnet_softmax))
-  colnames(preds) <- 'Predicted_ethnicity_nothresh'
+  colnames(preds) <- "Predicted_ethnicity_nothresh"
 
   # combine and apply thresholding
   p <- cbind(preds, probs)
-  p$Highest_Prob <- apply(p[,2:4], 1, max)
-  p$Predicted_ethnicity <- ifelse(p$Highest_Prob < threshold, 'Ambiguous',
-                                  as.character(p$Predicted_ethnicity_nothresh))
+  p$Highest_Prob <- apply(p[, 2:4], 1, max)
+  p$Predicted_ethnicity <- ifelse(p$Highest_Prob < threshold, "Ambiguous",
+    as.character(p$Predicted_ethnicity_nothresh)
+  )
   p$Sample_ID <- rownames(p)
-  p <- p[,c(7,1,6, 2:5)]
+  p <- p[, c(7, 1, 6, 2:5)]
 
   return(tibble::as_tibble(p))
 }
 
 # taken from glmnet v3.0.2 source
 glmnet_softmax <-
-  function (x,ignore_labels=FALSE)
-  {
+  function(x, ignore_labels = FALSE) {
     d <- dim(x)
     dd <- dimnames(x)[[2]]
-    if(is.null(dd) || !length(dd)) ignore_labels=TRUE
+    if (is.null(dd) || !length(dd)) ignore_labels <- TRUE
 
-    nas=apply(is.na(x),1,any)
-    if(any(nas)){
-      pclass=rep(NA,d[1])
-      if(sum(nas)<d[1]){
-        pclass2=glmnet_softmax(x[!nas,],ignore_labels)
-        pclass[!nas]=pclass2
-        if(is.factor(pclass2))pclass=factor(pclass,levels = seq(d[2]),labels=levels(pclass2))
+    nas <- apply(is.na(x), 1, any)
+    if (any(nas)) {
+      pclass <- rep(NA, d[1])
+      if (sum(nas) < d[1]) {
+        pclass2 <- glmnet_softmax(x[!nas, ], ignore_labels)
+        pclass[!nas] <- pclass2
+        if (is.factor(pclass2)) pclass <- factor(pclass, levels = seq(d[2]), 
+                                                 labels = levels(pclass2))
       }
     }
-    else{
+    else {
       maxdist <- x[, 1]
       pclass <- rep(1, d[1])
       for (i in seq(2, d[2])) {
@@ -109,7 +118,8 @@ glmnet_softmax <-
         maxdist[l] <- x[l, i]
       }
       dd <- dimnames(x)[[2]]
-      if(!ignore_labels) pclass=factor(pclass, levels = seq(d[2]), labels = dd)
+      if (!ignore_labels) pclass <- factor(pclass, levels = seq(d[2]), 
+                                           labels = dd)
     }
     pclass
   }
