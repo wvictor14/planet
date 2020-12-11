a004ced1eff77867c32dc0e3ec720e202d877610
Victor <victor.2wy@gmail.com>
2020-12-11 13:05:12 -0800

    styler
    Former-commit-id: 2b8b36ace2f4b0cc28eaee21858e33215c964a12 [formerly f6d5abd63e0cf84250ee2e35a1883e38514eae51] [formerly 9b6236e75796c73cf05157e8456773e409da96bd] [formerly 85f84b059406a5f4a04cc20592ed12da3a05952e [formerly b36464ead16adf809af292f5bc636476fd108269]]
    Former-commit-id: 64ebd0b8ce262a02c0548cc3fea747f482755140 [formerly daf519a109cbb1b1b0d52d62cde02e00ed8898a9] [formerly 3660313720c1d459ef76dda1939155b38fe920b1] [formerly 5ea2e8f14cc5a131719437be471323d0262cae55 [formerly e99ab1ae8279501dedca9f1d986ed481a9d3ad99]]
    Former-commit-id: 62f578dead5df108146fdcc049859de5cbae9c3b [formerly a0d4cb82c0cd0d5dff06b64d5ba91c05a37dbe6a]
    Former-commit-id: 71effc6bc1f21e5aaa1369bb821f7cba9b039d49
    Former-commit-id: af04f35e83bf4513a08322735e657633e2862d42

--- R/pl_infer_age.R
+++ R/pl_infer_age.R
@@ -1,63 +1,70 @@
 #' Predicts gestational age using DNA methylation microarray data
-#' 
-#' \code{pl_infer_age} Multiplies the coefficients from one of three epigenetic gestational age 
-#' clocks by the corresponding CpGs in a supplied betas `data.frame`. 
 #'
-#' @param betas An n by m dataframe of methylation values on the beta scale (0, 1), where the 
-#' CpGs are arranged in rows, and samples in columns. Should contain all CpGs used in each clock
-#' @param type One of the following: "RPC" (Robust), "CPC", (Control) or "RRPC" (Refined Robust). 
-#' 
-#' @details Predicts gestational age using one of 3 placental gestational age clocks: RPC, CPC, or 
-#' refined RPC. Requires placental DNA methylation measured on the Infinium 27K/450k/EPIC 
-#' methylation array. If CpGs are missing, a warning is returned. 
+#' \code{pl_infer_age} Multiplies the coefficients from one of three epigenetic
+#' gestational age clocks by the corresponding CpGs in a supplied betas
+#' `data.frame`.
 #'
-#' @return A vector of length `m`, containing inferred gestational age. `
-#' 
+#' @param betas An n by m dataframe of methylation values on the beta scale (0,
+#' 1), where the CpGs are arranged in rows, and samples in columns. Should
+#' contain all CpGs used in each clock
+#' @param type One of the following: "RPC" (Robust), "CPC", (Control) or "RRPC"
+#' (Refined Robust).
+#'
+#' @details Predicts gestational age using one of 3 placental gestational age
+#' clocks: RPC, CPC, or refined RPC. Requires placental DNA methylation measured
+#' on the Infinium 27K/450k/EPIC methylation array. If CpGs are missing, a
+#' warning is returned.
+#'
+#' @return A vector of length `m`, containing inferred gestational age.
+#'
 #' @examples
 #'
 #' # Load placenta DNAm data
 #' library(dplyr)
 #' data(pl_betas)
 #' data(pl_pDat)
-#' 
-#' pl_pDat %>%
-#'   mutate(inferred_ga = pl_infer_age(pl_betas, type = 'RPC'))
 #'
+#' pl_pDat %>%
+#'   mutate(inferred_ga = pl_infer_age(pl_betas, type = "RPC"))
 #' @export
-#' 
-pl_infer_age <- function(betas, type = 'RPC') {
-  
+#'
+pl_infer_age <- function(betas, type = "RPC") {
   RPC <- CPC <- RRPC <- CpGs <- NULL
-  
+
   # Filter to coefficients
-  if (type == 'RPC') {
-    coef <- planet::pl_clock %>% 
+  if (type == "RPC") {
+    coef <- planet::pl_clock %>%
       dplyr::filter(RPC != 0)
-  } else if (type == 'CPC') {
-    coef <- planet::pl_clock %>% 
+  } else if (type == "CPC") {
+    coef <- planet::pl_clock %>%
       dplyr::filter(CPC != 0)
-  } else if (type == 'RRPC') {
-    coef <- planet::pl_clock %>% 
+  } else if (type == "RRPC") {
+    coef <- planet::pl_clock %>%
       dplyr::filter(RRPC != 0)
   } else {
     stop('Type must be one of "CPC", "RPC", or "RRPC"')
   }
-  
-  cpgs <- intersect(coef %>% 
-                      dplyr::filter(CpGs != '(Intercept)') %>% 
-                      dplyr::pull(CpGs), 
-                    rownames(betas))
-  
+
+  cpgs <- intersect(
+    coef %>%
+      dplyr::filter(CpGs != "(Intercept)") %>%
+      dplyr::pull(CpGs),
+    rownames(betas)
+  )
+
   # Check if CpGs missing
-  if (length(cpgs) < (nrow(coef)-1)) {
-    warning(paste('Only', length(cpgs), 'out of', (nrow(coef)-1), 'predictors present.'))
+  if (length(cpgs) < (nrow(coef) - 1)) {
+    warning(paste(
+      "Only", length(cpgs), "out of", (nrow(coef) - 1),
+      "predictors present."
+    ))
   } else {
-    print(paste(length(cpgs), 'of', (nrow(coef)-1), 'predictors present.'))
+    print(paste(length(cpgs), "of", (nrow(coef) - 1), "predictors present."))
   }
-  
-  betas <- cbind(1, t(betas[cpgs,]))
-  age <- betas %*% (coef %>% dplyr::pull(!!type)) %>% 
-    as.vector
-  
+
+  betas <- cbind(1, t(betas[cpgs, ]))
+  age <- betas %*% (coef %>% dplyr::pull(!!type)) %>%
+    as.vector()
+
   return(age)
 }
