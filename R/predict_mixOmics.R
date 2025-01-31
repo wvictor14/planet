d32e51d6e3c15a6f3e27ff28e7a1d2d19b507f05
Victor Yuan <victor.2wy@gmail.com>
2025-01-30 23:23:39 -0800

    add other functions from mixOmics

--- R/predict_mixOmics.R
+++ R/predict_mixOmics.R
@@ -771,3 +771,472 @@ predict.mixo_pls <-
 #' @export
 predict.mixo_spls <- predict.mixo_pls
 
+
+# from https://github.com/mixOmicsTeam/mixOmics/blob/master/R/check_entry.R#L111
+################################################################################
+# --------------------------------------
+# Check.entry.single: check input parameter for a single matrix that comes
+#   from a list of matrix
+# --------------------------------------
+# X: a single numeric matrix
+# ncomp: the number of components to include in the model
+# q: position of X in a list of matrix, as used in horizontal analysis
+#   (e.g. block.pls)
+Check.entry.single = function(X,  ncomp, q)
+{
+  
+  #-- validation des arguments --#
+  if (length(dim(X)) != 2)
+    stop(paste0("'X[[", q, "]]' must be a numeric matrix."))
+  
+  if(! any(class(X) %in% "matrix"))
+    X = as.matrix(X)
+  
+  if (!is.numeric(X))
+    stop(paste0("'X[[", q, "]]'  must be a numeric matrix."))
+  
+  N = nrow(X)
+  P = ncol(X)
+  
+  if (is.null(ncomp) || !is.numeric(ncomp) || ncomp <= 0)
+    stop(paste0(
+      "invalid number of variates 'ncomp' for matrix 'X[[", q, "]]'."))
+  
+  ncomp = round(ncomp)
+  
+  # add colnames and rownames if missing
+  X.names = dimnames(X)[[2]]
+  if (is.null(X.names))
+  {
+    X.names = paste("X", 1:P, sep = "")
+    dimnames(X)[[2]] = X.names
+  }
+  
+  ind.names = dimnames(X)[[1]]
+  if (is.null(ind.names))
+  {
+    ind.names = 1:N
+    rownames(X)  = ind.names
+  }
+  
+  if (length(unique(rownames(X))) != nrow(X))
+    stop("samples should have a unique identifier/rowname")
+  if (length(unique(X.names)) != P)
+    stop("Unique indentifier is needed for the columns of X")
+  
+  return(list(X=X, ncomp=ncomp, X.names=X.names, ind.names=ind.names))
+}
+
+# from https://github.com/mixOmicsTeam/mixOmics/blob/master/R/logratio-transformations.R
+#' Log-ratio transformation
+#' 
+#' This function applies a log transformation to the data, either CLR or ILR
+#' 
+#' \code{logratio.transfo} applies a log transformation to the data, either CLR
+#' (centered log ratio transformation) or ILR (Isometric Log Ratio
+#' transformation). In the case of CLR log-transformation, X needs to be a
+#' matrix of non-negative values and \code{offset} is used to shift the values
+#' away from 0, as commonly done with counts data.
+#' 
+#' @param X numeric matrix of predictors
+#' @param logratio log-ratio transform to apply, one of "none", "CLR" or "ILR"
+#' @param offset Value that is added to X for CLR and ILR log transformation.
+#' Default to 0.
+#' @return \code{logratio.transfo} simply returns the log-ratio transformed
+#' data.
+#' @author Florian Rohart, Kim-Anh Lê Cao, Al J Abadi
+#' @seealso \code{\link{pca}}, \code{\link{pls}}, \code{\link{spls}},
+#' \code{\link{plsda}}, \code{\link{splsda}}.
+#' @references Kim-Anh Lê Cao, Mary-Ellen Costello, Vanessa Anne Lakis,
+#' Francois Bartolo, Xin-Yi Chua, Remi Brazeilles, Pascale Rondeau mixMC: a
+#' multivariate statistical framework to gain insight into Microbial
+#' Communities bioRxiv 044206; doi: http://dx.doi.org/10.1101/044206
+#' 
+#' John Aitchison. The statistical analysis of compositional data. Journal of
+#' the Royal Statistical Society. Series B (Methodological), pages 139-177,
+#' 1982.
+#' 
+#' Peter Filzmoser, Karel Hron, and Clemens Reimann. Principal component
+#' analysis for compositional data with outliers. Environmetrics,
+#' 20(6):621-632, 2009.
+#' @examples
+#' 
+#' data(diverse.16S)
+#' CLR = logratio.transfo(X = diverse.16S$data.TSS, logratio = 'CLR')
+#' # no offset needed here as we have put it prior to the TSS, see www.mixOmics.org/mixMC
+#' @name logratio-transformations
+NULL
+#' @rdname logratio-transformations
+#' @export
+logratio.transfo <- function(X,
+                             logratio = c('none','CLR','ILR'),
+                             offset = 0)
+{
+  X <- as.matrix(X)
+  if (!is.numeric(X))
+    stop("X must be a numeric matrix", call. = FALSE)
+  
+  logratio <- match.arg(logratio)
+  if (logratio == 'ILR')
+  {
+    if (!is(X, 'ilr'))
+    {   # data are ilr transformed, then the data lose 1 variable, but we'll use V to reconstruct the matrix
+      X = ilr.transfo(X, offset = offset)
+    }
+  } else if (logratio == 'CLR') {
+    X = clr.transfo(X, offset = offset)
+  }
+  return(X)
+}
+
+
+#' @importFrom dplyr group_by row_number filter n
+internal_predict.DA <- 
+  function(object, out, q, dist, weights)
+  {
+    # a DA analysis (mint).(block).(s)plsda
+    if (!is(object, "DA"))
+      stop("'Object' is not from a Discriminant Analysis", call.=FALSE)
+    
+    out.DA = list()
+    J = length(object$X) #at this stage we have a list of blocks
+    p = lapply(object$X, ncol)
+    t.pred = out$variates
+    Y.hat = out$predict
+    newdata = out$newdata #actually concat.newdata
+    variatesX = object$variates[-(J + 1)];
+    ncomp = object$ncomp
+    
+    Y = object$Y
+    Y.prim = unmap(object$Y)
+    G = cls = list()
+    for (i in 1 : J)
+    {
+      G[[i]] = sapply(1:q, function(x)
+      {apply(as.matrix(variatesX[[i]][Y.prim[, x] == 1,
+                                      ,drop=FALSE]), 2, mean)})
+      if (ncomp[i] == 1)
+        G[[i]] = t(t(G[[i]]))
+      else
+        G[[i]] = t(G[[i]])
+      colnames(G[[i]]) = paste0("dim", c(1:ncomp[i]))
+      rownames(G[[i]]) = colnames(object$ind.mat)
+      
+    }
+    names(G)=names(object$X)
+    
+    ### Start: Maximum distance
+    if (any(dist == "all") || any(dist == "max.dist"))
+    {
+      cls$max.dist = lapply(1:J, function(x){matrix(sapply(1:ncomp[x],
+                                                           ### List level
+                                                           function(y){apply(Y.hat[[x]][, , y, drop = FALSE], 1,
+                                                                             ### component level
+                                                                             function(z){
+                                                                               paste(levels(Y)[which(z == max(z))], collapse = "/")
+                                                                             }) ### matrix level
+                                                           }), nrow = nrow(newdata[[x]]), ncol = ncomp[x])
+      })
+      cls$max.dist = lapply(1:J, function(x){colnames(cls$max.dist[[x]]) =
+        paste0(rep("comp", ncomp[x]), 1 : ncomp[[x]]);
+      rownames(cls$max.dist[[x]]) = rownames(newdata[[x]]);
+      return(cls$max.dist[[x]])})
+      names(cls$max.dist)=names(object$X)
+    }
+    
+    
+    ### Start: Centroids distance
+    if (any(dist == "all") || any(dist == "centroids.dist"))
+    {
+      cl = list()
+      centroids.fun = function(x, G, h, i) {
+        q = nrow(G[[i]])
+        x = matrix(x, nrow = q, ncol = h, byrow = TRUE)
+        
+        if (h > 1) {
+          d = apply((x - G[[i]][, 1:h])^2, 1, sum)
+        }
+        else {
+          d = (x - G[[i]][, 1])^2
+        }
+        cl.id = paste(levels(Y)[which(d == min(d))], collapse = "/")
+      }
+      
+      for (i in 1 : J)
+      {
+        cl[[i]] = matrix(nrow = nrow(newdata[[1]]), ncol = ncomp[i])
+        
+        for (h in 1 : ncomp[[i]])
+        {
+          cl.id = apply(matrix(t.pred[[i]][, 1:h], ncol = h), 1,
+                        function(x) {centroids.fun(x = x, G = G, h = h, i = i)})
+          cl[[i]][, h] = cl.id
+        }
+      }
+      
+      cls$centroids.dist = lapply(1:J, function(x){colnames(cl[[x]]) =
+        paste0(rep("comp", ncomp[x]), 1 : ncomp[[x]]);
+      rownames(cl[[x]]) = rownames(newdata[[x]]); return(cl[[x]])})
+      names(cls$centroids.dist)=names(object$X)
+    }### End: Centroids distance
+    
+    
+    ### Start: Mahalanobis distance
+    if (any(dist == "all") || any(dist == "mahalanobis.dist"))
+    {
+      cl = list()
+      Sr.fun = function(x, G, Yprim, h, i) {
+        q = nrow(G[[i]])
+        Xe = Yprim %*% G[[i]][, 1:h]
+        #Xr = object$variates$X[, 1:h] - Xe
+        Xr = variatesX[[i]][, 1:h] - Xe
+        Sr = t(Xr) %*% Xr/nrow(Yprim)
+        Sr.inv = solve(Sr)
+        x = matrix(x, nrow = q, ncol = h, byrow = TRUE)
+        if (h > 1) {
+          mat = (x - G[[i]][, 1:h]) %*% Sr.inv %*% t(x - G[[i]][, 1:h])
+          d = apply(mat^2, 1, sum)
+        } else {
+          d = drop(Sr.inv) * (x - G[[i]][, 1])^2
+        }
+        cl.id = paste(levels(Y)[which(d == min(d))], collapse = "/")
+      }
+      
+      for (i in 1 : J){
+        cl[[i]] = matrix(nrow = nrow(newdata[[1]]), ncol = ncomp[i])
+        
+        for (h in 1:ncomp[[i]]) {
+          cl.id = apply(matrix(t.pred[[i]][, 1:h], ncol = h), 1, Sr.fun,
+                        G = G, Yprim = Y.prim, h = h, i = i)
+          cl[[i]][, h] = cl.id
+        }
+      }
+      
+      cls$mahalanobis.dist = lapply(1:J, function(x){colnames(cl[[x]]) =
+        paste0(rep("comp", ncomp[x]), 1 : ncomp[[x]]);
+      rownames(cl[[x]]) = rownames(newdata[[x]]);return(cl[[x]])})
+      names(cls$mahalanobis.dist)=names(object$X)
+    } ### End: Mahalanobis distance
+    
+    out.DA$class = cls
+    
+    ### End if discriminant analysis is performed
+    
+    # at this stage, we have the classification of each sample for each dataset
+    #   of object$X
+    # now we need to combine the classification by vote (majority wins),
+    #  only when more than one block, otherwise 'vote' is classic classification
+    if (length(object$X)>1)
+    {
+      for (ijk in 1:length(out.DA$class))# loop on the dist
+      {
+        # create a temporary array to make computation on the lists easier
+        temp=array(, c(nrow(newdata[[1]]), min(ncomp), J))
+        for(i in 1:J)
+        {
+          temp[, , i] = out.DA$class[[ijk]][[i]][, 1:min(ncomp)]
+          
+        }
+        # look at the majority vote for all dataset of object$X (with table)
+        #   if more than a unique max, we put NA
+        table.temp = apply(temp,c(1,2), function(x)
+        {a=table(x); if (length(which(a==max(a)))==1)
+        {b=names(which.max(a))}else{b=NA}; b})
+        colnames(table.temp) =
+          colnames(out.DA$class[[ijk]][[i]])[1:min(ncomp)]
+        rownames(table.temp) = rownames(out.DA$class[[ijk]][[i]])
+        out.DA$MajorityVote[[ijk]] = table.temp
+      }
+      names(out.DA$MajorityVote) = names(out.DA$class)
+      
+      #save(list=ls(),file="temp.Rdata")
+      #stop("")
+      # weighted vote for each distance, each comp
+      if(!is.null(weights))
+      {
+        out.WeightedVote = vector("list",length=length(out.DA$class))
+        Group.2 = n = x =NULL #CRAN check
+        for(i in 1:length(out.DA$class)){ #i distance
+          out = matrix(NA_real_,nrow=nrow(newdata[[1]]), ncol=min(ncomp))
+          rownames(out) = rownames(newdata[[1]])
+          colnames(out) = paste0("comp",1:min(ncomp))
+          
+          for(comp in 1:min(ncomp)){ #comp
+            data.temp=NULL
+            for(j in 1:J){ #block
+              data.temp = rbind(data.temp,
+                                out.DA$class[[i]][[j]][,comp,drop=FALSE])
+            }
+            colnames(data.temp)="pred"
+            temp=data.frame(data.temp,indiv=rownames(data.temp),
+                            weights=rep(weights,each=nrow(out.DA$class[[1]][[1]])))
+            ag = aggregate(temp$weights,
+                           by=list(temp$pred, temp$indiv), FUN=sum)
+            data_max <- group_by(.data = ag, Group.2)
+            data_max <- filter(.data = data_max, row_number(x)==n())
+            out.comp = as.matrix(data_max[,1])
+            rownames(out.comp) = as.matrix(data_max[,2])
+            colnames(out.comp) = paste0("comp",comp)
+            
+            out[,comp] = out.comp[match(rownames(out),
+                                        rownames(out.comp)),]
+          }
+          
+          out.WeightedVote[[i]] = out
+        }
+        names(out.WeightedVote) = names(out.DA$class)
+        out.DA$WeightedVote = out.WeightedVote
+        
+        
+        
+        if(FALSE){
+          out.DA$WeightedVote = lapply(out.DA$class, function(x){
+            # x is a distance
+            class.per.comp = lapply(1:min(ncomp), function(y) {
+              matrix(sapply(x, function(z)
+                z[,y, drop = FALSE]),ncol=J)})
+            # combine the results per component
+            names(class.per.comp) = paste0("comp",1:min(ncomp))
+            class.weighted.per.comp = sapply(class.per.comp, function(y){
+              # for each component
+              apply(y,1,function(z){
+                # we aggregate the results of each individuals using the 'weights'
+                temp = aggregate(weights,list(as.character(z)),sum)
+                ind = which(temp[,2]== max (temp[,2]))
+                # if two max, then NA
+                if(length(ind) == 1)
+                {
+                  res = as.character(temp[ind, 1])
+                } else {
+                  res = NA
+                }
+                res
+                
+              })
+              
+            })
+            class.weighted.per.comp = matrix(class.weighted.per.comp,
+                                             nrow = nrow(class.per.comp[[1]]))
+            colnames(class.weighted.per.comp) = names(class.per.comp)
+            rownames(class.weighted.per.comp) =
+              rownames(out.DA$MajorityVote[[1]])
+            class.weighted.per.comp
+            
+          })
+        }
+        out.DA$weights = weights
+        
+      }
+    }else{
+      out.DA$MajorityVote = lapply(out.DA$class,function(x){x[[1]]})
+    }
+    
+    block.object = c("block.pls", "block.spls", "block.plsda", "block.spsda")
+    if (inherits(object, block.object) & J>1) # a block
+    {
+      out.DA$centroids = G
+    }else{ #not a block
+      out.DA$centroids = G[[1]]
+      out.DA$class = out.DA$MajorityVote
+    }
+    if (any(dist == "all"))
+      dist = "all"
+    
+    out.DA$dist = dist
+    
+    out.DA
+    
+  }
+
+# from https://github.com/mixOmicsTeam/mixOmics/blob/766521eaff07b25c9c2ceccdabdf281dd0ef735c/R/unmap.R#L47
+# ---------------------------------------------------
+# unmap variates.A variable for (s)plsda
+# ---------------------------------------------------
+#' Dummy matrix for an outcome factor
+#' 
+#' Converts a class or group vector or factor into a matrix of indicator
+#' variables.
+#' 
+#' @param classification A numeric or character vector or factor. Typically the
+#' distinct entries of this vector would represent a classification of
+#' observations in a data set.
+#' @param groups A numeric or character vector indicating the groups from which
+#' \code{classification} is drawn. If not supplied, the default is to assumed
+#' to be the unique entries of classification.
+#' @param noise A single numeric or character value used to indicate the value
+#' of \code{groups} corresponding to noise.
+#' @return An \emph{n} by \emph{K} matrix of \emph{(0,1)} indicator variables,
+#' where \emph{n} is the length of samples and \emph{K} the number of classes
+#' in the outcome.
+#' 
+#' If a \code{noise} value of symbol is designated, the corresponding indicator
+#' variables are relocated to the last column of the matrix.
+#' 
+#' Note: - you can remap an unmap vector using the function \code{map} from the
+#' package \pkg{mclust}. - this function should be used to unmap an outcome
+#' vector as in the non-supervised methods of mixOmics. For other supervised
+#' analyses such as (s)PLS-DA, (s)gccaDA this function is used internally.
+#' @author Ignacio Gonzalez, Kim-Anh Le Cao, Pierre Monget, AL J Abadi
+#' @references 
+#' C. Fraley and A. E. Raftery (2002). Model-based
+#' clustering, discriminant analysis, and density estimation. \emph{Journal of
+#' the American Statistical Association 97:611-631}.
+#' 
+#' C. Fraley, A. E. Raftery, T. B. Murphy and L. Scrucca (2012). mclust Version
+#' 4 for R: Normal Mixture Modeling for Model-Based Clustering, Classification,
+#' and Density Estimation. Technical Report No. 597, Department of Statistics,
+#' University of Washington.
+#' @keywords cluster
+#' @export
+#' @examples
+#' data(nutrimouse)
+#' Y = unmap(nutrimouse$diet)
+#' Y
+#' data = list(gene = nutrimouse$gene, lipid = nutrimouse$lipid, Y = Y)
+#' # data could then used as an input in wrapper.rgcca, which is not, technically,
+#' # a supervised method, see ??wrapper.rgcca
+unmap <-
+  function (classification, groups = NULL, noise = NULL)
+  {
+    n = length(classification)
+    u = sort(unique(classification))
+    levels =  levels(classification)### Add levels
+    
+    if (is.null(groups))
+    {
+      groups = u
+    } else {
+      if (any(match(u, groups, nomatch = 0) == 0))
+        stop("groups incompatible with classification")
+      miss = match(groups, u, nomatch = 0) == 0
+    }
+    
+    cgroups = as.character(groups)
+    if (!is.null(noise))
+    {
+      noiz = match(noise, groups, nomatch = 0)
+      if (any(noiz == 0))
+        stop("noise incompatible with classification")
+      
+      groups = c(groups[groups != noise], groups[groups == noise])
+      noise = as.numeric(factor(as.character(noise), levels = unique(groups)))
+    }
+    
+    groups = as.numeric(factor(cgroups, levels = unique(cgroups)))
+    classification = as.numeric(factor(as.character(classification), levels = unique(cgroups)))
+    k = length(groups) - length(noise)
+    nam = levels(groups)
+    
+    if (!is.null(noise))
+    {
+      k = k + 1
+      nam = nam[1:k]
+      nam[k] = "noise"
+    }
+    
+    z = matrix(0, n, k, dimnames = c(names(classification), nam))
+    for (j in 1:k) z[classification == groups[j], j] = 1
+    attr(z, "levels") = levels
+    z
+  }
+
